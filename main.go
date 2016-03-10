819af7f8c27e59dda283602b1126724a2ecdf8b7
Alfonso Cabrera <a.l.cabrera11@gmail.com>
2016-03-10 15:23:19 -0500

    Update main.go and remove custom strings

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/elb"
 	"github.com/iamthemuffinman/logsip"
@@ -15,39 +16,42 @@ var log = logsip.New(os.Stdout)
 
 func main() {
 	// Set config options.
-	awsProfile := ""
-	sourceElb := ""
-	destElb := ""
+	sourceElb := "<sourceELB_name_here>"
+	destElb := "<destELB_name_here>"
 
-	// Set AWS_PROFILE env variable on OS.
-	err := os.Setenv("AWS_PROFILE", awsProfile)
-	if err != nil {
-		log.Fatal("Failed to set AWS_PROFILE environment variable", err)
-	}
-	// Open a new elb session with the aws-sdk.
-	svc := elb.New(session.New())
+	// Create a new aws session.
+	stsSvc := session.New()
+
+	// Get new temporary STS credentials for assumed role.
+	getCreds := stscreds.NewCredentials(stsSvc, "<roleARNhere>")
+
+	// Open a new elb session with the aws-sdk and pass in temporary sts credentials.
+	svc := elb.New(session.New(&aws.Config{Region: aws.String("us-east-1"), Credentials: getCreds}))
 
 	// Define parameters to pass to DescribeInstanceHealth
 	params := &elb.DescribeInstanceHealthInput{LoadBalancerName: aws.String(sourceElb), Instances: []*elb.Instance{}}
 
 	result, err := svc.DescribeInstanceHealth(params)
 	if err != nil {
-		log.Fatal("Failed to describe ELBs", err)
+		log.Fatal("Failed to describe ELB: \n", err.Error())
 	}
+
 	// See instances currently registered with sourceElb.
+	fmt.Printf("Currently registered instances with %s: \n", sourceElb)
 	fmt.Println(result)
 
 	// Loop through registered instances to get instance ids and register them with the destElb.
 	for _, instances := range result.InstanceStates {
 
 		id := aws.StringValue(instances.InstanceId)
-		resp, err := svc.RegisterInstancesWithLoadBalancer(&elb.RegisterInstancesWithLoadBalancerInput{Instances: []*elb.Instance{{InstanceId: aws.String(id)}}, LoadBalancerName: aws.String(destElb)})
+		_, err := svc.RegisterInstancesWithLoadBalancer(&elb.RegisterInstancesWithLoadBalancerInput{Instances: []*elb.Instance{{InstanceId: aws.String(id)}}, LoadBalancerName: aws.String(destElb)})
 		if err != nil {
 			log.Fatal("Failed to register instance", err.Error())
 		}
-		fmt.Println(resp)
-	}
 
+	}
+	// Sleep for 20 seconds to allow instance registration in the destElb.
+	fmt.Println("Going to sleep for 20 seconds before checking instance registration status ... ")
 	time.Sleep(20 * time.Second)
 
 	// Define parameters to pass to DescribeInstanceHealth for destElb
@@ -69,3 +73,4 @@ func main() {
 	}
 
 }
+
